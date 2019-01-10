c728c629bfc7770945ad3410e760cf13b46885d8
kishimoto <kissy0613@gmail.com>
2019-01-11 03:59:05 +0900

    ok

--- app/src/main/java/com/example/kishimoto/tekitou/CardRecyclerView.java
+++ app/src/main/java/com/example/kishimoto/tekitou/CardRecyclerView.java
@@ -6,15 +6,37 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
 public class CardRecyclerView extends RecyclerView {
     public CardRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setRecyclerAdapter(context);
+        Realm.init(getContext());
+        Realm realm = Realm.getDefaultInstance();
+        setRecyclerAdapter(context,realm);
     }
 
-    public void setRecyclerAdapter(Context context){
+    public void setRecyclerAdapter(Context context,Realm realm){
         setLayoutManager(new LinearLayoutManager(context));
         setLayoutManager(new GridLayoutManager(context,2));
-        setAdapter(new CardRecyclerAdapter(context,context.getResources().getStringArray(R.array.dummy)));
+
+        ArrayList<String> taskList = new ArrayList<String>();
+
+        RealmResults<TaskText> Task = realm.where(TaskText.class).findAll();
+        for(int i = 0;i <= Task.size();i++){
+            TaskText taskmemo = Task.where().equalTo("id",i).findFirst();
+            try {
+                taskList.add(taskmemo.getTaskText());
+            } catch (NullPointerException e){
+
+            }
+        }
+
+        String[] array = taskList.toArray(new String[Task.size()]);
+
+        setAdapter(new CardRecyclerAdapter(context,array));
     }
 }
--- app/src/main/java/com/example/kishimoto/tekitou/InputMemoActivity.java
+++ app/src/main/java/com/example/kishimoto/tekitou/InputMemoActivity.java
@@ -1,11 +1,18 @@
 package com.example.kishimoto.tekitou;
 
-import android.support.annotation.Nullable;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Xml;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import org.xmlpull.v1.XmlSerializer;
 
@@ -13,8 +20,12 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class InputMemoActivity extends AppCompatActivity {
 
@@ -23,27 +34,90 @@ public class InputMemoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_memo);
     }
+
     public void onClick(View v){
         EditText editText = (EditText)findViewById(R.id.editText);
         String memoText = editText.getText().toString();
 
+        writeXml(memoText);
+
+        Intent intent = new Intent(InputMemoActivity.this, MainActivity.class);
+        startActivity(intent);
 
     }
 
-    public void writeXml(@Nullable int id, String memoText) {
+    public void onClickBack(View v){
+        Intent intent = new Intent(InputMemoActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickReminder(View v){
+        final Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog dialog = new TimePickerDialog(
+                this,
+                new TimePickerDialog.OnTimeSetListener(){
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("test",String.format("%02d:%02d", hourOfDay,minute));
+                        Calendar calender = Calendar.getInstance();
+                        int hourofnow = calender.get(Calendar.HOUR_OF_DAY);
+                        int minuteofnow = calender.get(Calendar.MINUTE);
+                        int totalminute = (hourofnow * 60 + minuteofnow) - (hourOfDay * 60 + minute);
+                        int hourofalarm = totalminute / 60;
+                        int minuteofalarm = totalminute % 60;
+
+                        Calendar alarmCalendar = Calendar.getInstance();
+                        alarmCalendar.setTimeInMillis(System.currentTimeMillis());
+                        alarmCalendar.add(Calendar.HOUR_OF_DAY, hourofalarm);
+                        alarmCalendar.add(Calendar.MINUTE,minuteofalarm);
+                        scheduleNotification(String.valueOf(hourofalarm) + "時間" + String.valueOf(minuteofalarm) + "分後にセットしました", alarmCalendar);
+                        Log.d("aaa",String.valueOf(hourofalarm) + "時間" + String.valueOf(minuteofalarm) + "分後にセットしました");
+                    }
+                },
+                hour,minute,true);
+        dialog.show();
+    }
+
+    private void scheduleNotification(String content, Calendar calendar){
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
+        notificationIntent.putExtra(AlarmReceiver.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(AlarmReceiver.NOTIFICATION_CONTENT, content);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+    public void writeXml(String memoText) {
         final Realm realm = Realm.getDefaultInstance();
-        if (id == null) {
-            id = makeId();
-        }
-        Recipe recipe = new Recipe();
-        recipe.setId(recipeId);
-        recipe.setName(recipeName);
-        recipe.setCookingTime(cookingTime);
+        int id = makeId(realm);
+        realm.beginTransaction();
+        TaskText taskText = realm.createObject(TaskText.class,id);
+        taskText.setTaskText(memoText);
+        realm.copyToRealmOrUpdate(taskText);
 
-        mRealm.beginTransaction();
-
-        mRealm.copyToRealmOrUpdate(recipe);
-
-        mRealm.commitTransaction();
+        realm.commitTransaction();
     }
+
+    public void writeXml(int id, String memoText) {
+        final Realm realm = Realm.getDefaultInstance();
+        TaskText taskText = realm.createObject(TaskText.class);
+        taskText.setId(id);
+        taskText.setTaskText(memoText);
+        realm.beginTransaction();
+
+        realm.copyToRealmOrUpdate(taskText);
+
+        realm.commitTransaction();
+    }
+
+    public int makeId(Realm realm) {
+        RealmResults<TaskText> results = realm.where(TaskText.class).findAll();
+        int id = results.size();
+        return id;
+    }
+
+
 }
